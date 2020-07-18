bbc0339bca928d3a8afd1e4a39d18ca1bcffc43a
Harsh Kapadia <harshgkapadia@gmail.com>
2020-07-18 13:40:08 +0530

    :hammer: refactor: Modify `burger_menu_dropdown.js` and `navbar.css`

--- static/js/burger_menu_dropdown.js
+++ static/js/burger_menu_dropdown.js
@@ -1,38 +1,21 @@
 const ul = document.querySelector(".navbar-main-ul");
 const burger_menu = document.querySelector(".burger-menu");
 
-window.addEventListener
-(
-    "load",
-    () =>
-    {
-        const width = document.documentElement.clientWidth;
-
-        if(width <= 1183)
-            ul.classList.add("hide");
-    }
-);
-
-window.addEventListener
-(
-    "resize",
-    () =>
-    {
-        const width = document.documentElement.clientWidth;
-
-        if(width <= 1183)
-            ul.classList.add("hide");
-        else
-           ul.classList.remove("hide");
-    }
-);
-
 burger_menu.addEventListener
+(
+    "click",
+    (event) =>
+    {
+        ul.classList.toggle("hide-burger-menu");
+        event.stopPropagation();
+    }
+);
+
+window.addEventListener
 (
     "click",
     () =>
     {
-        ul.classList.toggle("hide");
-        ul.classList.toggle("burger-menu-dropdown");
+        ul.classList.add("hide-burger-menu");
     }
 );
